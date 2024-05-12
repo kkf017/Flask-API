de28276204b13be2da3196f0191197ed563f285a
kkf17 <favre.kathleen@orange.fr>
2024-05-12 13:02:47 +0200

    Updt config.py

--- services/config.py
+++ services/config.py
@@ -4,9 +4,9 @@ import flask
 
 IMG_ALLOWED = {'png', 'jpg', 'jpeg', 'JPG'}#'JPG', 'gif'}
 
-TEMPLATE = "/home/ksys/Documents/app/app1/template"
-STATIC = "/home/ksys/Documents/app/app1/static"
-DATA = "/home/ksys/Documents/app/app1/static/data"
+TEMPLATE = "" # path for template folder (ex. "/home/Username/Documents/app/app1/template")
+STATIC = "" # path for static folder (ex. "/home/Username/Documents/app/app1/static")
+DATA = "" # path for data folder (ex. "/home/Username/Documents/app/app1/static/data")
 
 app = flask.Flask(__name__, template_folder=TEMPLATE, static_folder=STATIC)
 app.config['UPLOAD'] = os.path.join(STATIC, 'upload')
